622822a5dd3703b5a0e512a8a029de09616383e4
KLusvarghi <kauaolusvarghi@gmail.com>
2025-04-04 22:23:58 -0300

    feat(error): meddlware que trata a maioria dos erros

--- src/middlewares/errorHandling.js
+++ src/middlewares/errorHandling.js
@@ -1,19 +1,24 @@
 import mongoose from "mongoose";
+import BaseError from "../errors/BaseError.js";
+import IncorrectRequest from "../errors/IncorrectRequest.js";
+import ValidationError from "../errors/ValidationErro.js";
+import NotFound from "../errors/NotFound.js";
 
-function errorHandling(error, req, res, next){
-  // é um dado especifico de quando o mongoose não esta esperando, que no caso é um caracter não suportado pelo ObjectId do mongoose
-  if (error instanceof mongoose.Error.CastError) {
-    res.status(400).send({ message: "Um ou mais dados fornecidos estão incorretos" });
-  } 
-  // sendo esse um tipo de erro quando a informação requerida pelo nosso schema não é encontrada
-  else if(error instanceof mongoose.Error.ValidationError){
-    // dentro de "erro.error" nos temos acesso aos erros de validação do mongoose "ValidationError"
-    const messageError = Object.values(error.errors)
-    .map(erro => erro.message) // neste caso ele irá retornar [ 'Path `nome` is required.' ]
-    console.log(messageError)
-    // res.status(400).send({ message: "Erro na validação dos dados enviados" });
+function errorHandling(erro, req, res, next) {
+  // O erro "CastError" no Mongoose é um erro bastante comum que ocorre quando o Mongoose tenta converter (ou fazer "cast") um valor para um tipo específico definido no schema, mas não consegue realizar essa conversão corretamente.
+  // Como exemplo: id inválido, tipos incompativeis, valores inválidos em arrays, etc
+  if (erro instanceof mongoose.Error.CastError) {
+    new IncorrectRequest().sendResponse(res)
+  }
+
+  // é um tipo de erro que ocorre quando a validação de um documento falha durante operações de salvamento (save()) ou atualização.
+  // Este erro acontece quando os dados que você está tentando salvar ou atualizar não atendem às regras de validação definidas no seu schema.
+  else if (erro instanceof mongoose.Error.ValidationError) {
+    new ValidationError(erro).sendResponse(res)
+  } else if(erro instanceof NotFound){
+    erro.sendResponse(res) // e como o objeto NOtFound foi extendido de "BaseError", o obj "erro" já possui o método sendResponse
   }else {
-    res.status(500).send({ message: `${error.message} - Erro interno no servidor` });
+    new BaseError().sendResponse(res)
   }
 }
 
@@ -21,4 +26,4 @@ export default errorHandling;
 
 
 // MIDDLEWARE 
-  // Eles sõa funçõe sque basicamente interceptam uma ação / Requisição
+// Eles sõa funçõe sque basicamente interceptam uma ação / Requisição
